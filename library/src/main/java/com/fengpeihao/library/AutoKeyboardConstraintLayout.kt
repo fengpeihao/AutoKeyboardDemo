3dfd4bc2e772d44d44dc4c8f66020a6306e3604e
zhpanvip <zhpanvip@outlook.com>
2021-02-03 19:19:22 +0800

    add library module.

--- library/src/main/java/com/fengpeihao/library/AutoKeyboardConstraintLayout.kt
+++ library/src/main/java/com/fengpeihao/library/AutoKeyboardConstraintLayout.kt
@@ -1,4 +1,4 @@
-package com.cfxc.autokeyboarddemo.autokeyboard
+package com.fengpeihao.library
 
 import android.annotation.SuppressLint
 import android.content.Context
@@ -13,7 +13,6 @@ import androidx.core.view.get
 import androidx.core.widget.NestedScrollView
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.RecyclerView
-import com.cfxc.autokeyboarddemo.R
 
 
 /**
